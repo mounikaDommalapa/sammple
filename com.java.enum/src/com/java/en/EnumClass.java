b15dc3641a9079ce7aaf238dc8aaedbbd0042ca1
Prashanth <prashanth0340@gmail.com>
2019-05-28 11:39:47 +0530

    Commit

---
+++ com.java.enum/src/com/java/en/EnumClass.java
@@ -0,0 +1,30 @@
+package com.java.en;
+
+enum Day{
+	MON,TUE,WD,THU,FRI,SAT,SUN
+}
+public class EnumClass {
+
+	Day day;
+	public EnumClass(Day day)
+	{
+		this.day=day;
+	}
+	public void daydisplay()
+	{
+		switch(day)
+		{
+		case MON:System.out.println("monday");
+					break;
+
+		case TUE:System.out.println("tuesday");
+				break;
+		case WD:System.out.println("wednesdday");
+		break;
+		case THU:System.out.println("thursday");
+break;
+		case FRI:System.out.println("friday");
+		}
+	}
+
+}
